323d863c3c9e9d9cda76f187d4e0749f8bb61fa2
javacafee <learnings@javacafee.com>
2023-09-24 23:16:57 +0530

    first_commit_24_sept

---
+++ src/main/java/org/etail/EtailSystemApplication.java
@@ -0,0 +1,13 @@
+package org.etail.etail_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtailSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtailSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/etail/etail_system/EtailSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.etail.etail_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtailSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

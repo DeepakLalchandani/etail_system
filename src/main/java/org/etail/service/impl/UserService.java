ebbe75d13493033b6c814319e4e9642e07a7d122
javacafee <learnings@javacafee.com>
2023-09-24 23:45:05 +0530

    etail_system_commit_4_24Sept

--- src/main/java/org/etail/service/impl/UserService.java
+++ src/main/java/org/etail/service/impl/UserService.java
@@ -2,7 +2,6 @@ package org.etail.service.impl;
 
 import org.etail.dao.IUserDAO;
 import org.etail.model.dto.UserDTO;
-import org.etail.repository.UserRepository;
 import org.etail.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
